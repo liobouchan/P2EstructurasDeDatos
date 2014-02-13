c7289380579385765d96f96ac6e2e218a0a21995
liobouchan <leo.bouchan@gmail.com>
2014-02-13 01:05:11 -0600

    Memoria dinamica para puestos

--- main.c
+++ main.c
@@ -2,10 +2,20 @@
 #include <curses.h>
 
 int main() {
+  //Declaración de variables que vamos a usar.
+  char *puesto;
+
+  //Usando memoria dinámica.
+  puesto=(char*)malloc(20*sizeof(char));
+
+  //Imprimiendo mensajes de Bienvenida al usuario.
   printf("%s\n", "Hola bienvenido" );
   printf("%s\n", "Indicanos tu puesto");
   printf("%s\n", " ");
   printf("%s\n", " ");
   printf("%s\n", "(A) Administrador");
   printf("%s\n", "(V) Vendedor");
+  gets(puesto);
+
+
 }
