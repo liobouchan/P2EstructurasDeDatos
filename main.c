9986e8f6ecad6806da51513de68a85397d2f8079
liobouchan <leo.bouchan@gmail.com>
2014-02-13 00:57:58 -0600

    Titulo de Admin o Vendedor

--- main.c
+++ main.c
@@ -4,4 +4,8 @@
 int main() {
   printf("%s\n", "Hola bienvenido" );
   printf("%s\n", "Indicanos tu puesto");
+  printf("%s\n", " ");
+  printf("%s\n", " ");
+  printf("%s\n", "(A) Administrador");
+  printf("%s\n", "(V) Vendedor");
 }
